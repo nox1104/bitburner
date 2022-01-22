ba20bc238b33aecbaaef8809501289cb2eb4d546
nox1104 <nox1104@mailbox.org>
2022-01-22 19:56:02 +0100

    own: alle programme hinzugefügt + purchase-server

--- src/own.java
+++ src/own.java
@@ -1,10 +1,16 @@
 var target =  args[0];
 
- if (fileExists("BruteSSH.exe", "home")){
-   brutessh(target);
- }
- if (fileExists("FTPcrack.exe", "home")){
-   ftpcrack(target);
- }
+ {
+	if (fileExists("BruteSSH.exe"))
+		brutessh(target);
+	if (fileExists("FTPCrack.exe"))
+		ftpcrack(target);
+	if (fileExists("relaySMTP.exe"))
+		relaysmtp(target);
+	if (fileExists("HTTPWorm.exe"))
+		httpworm(target);
+	if (fileExists("SQLInject.exe"))
+		sqlinject(target);
 
  nuke(target);
+}
