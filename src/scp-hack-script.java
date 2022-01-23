1f50d24d01ae2ea5c863bcc4b2e36449947aeb56
nox1104 <nox1104@mailbox.org>
2022-01-23 17:01:07 +0100

    funktionierende Vereinfachung

--- src/scp-hack-script.java
+++ src/scp-hack-script.java
@@ -1,15 +1,19 @@
-servers = ["foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea", "harakiri-sushi"];
+servers = ["foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea",
+	"harakiri-sushi"];
 
 i = 0;
 while (i < servers.length) {
+	scp("hack.script", servers[i]);
 
-	var ServerRam = getServerRam(servers[i]);
-	var ScriptRam = getScriptRam("hack.script", "home");
-	var NumberThreads = Math.round(ServerRam / ScriptRam);
-
-    scp("hack.script", servers[i]);
-
-    exec("hack.script", servers[i], NumberThreads, servers[i]);
-
-    ++i;
+    exec("hack.script", servers[i], 9, servers[i]);
+   ++i
 }
+
+
+
+//	var ServerRam = getServerRam(servers[i]);
+//	var ScriptRam = getScriptRam("hack.script", "home");
+//	var NumberThreads = Math.round(ServerRam / ScriptRam);
+//    scp("hack.script", servers[i]);
+
+//    exec("hack.script", servers[i], NumberThreads, servers[i]);
